788c347a8ad4d41429b1c5140ca7f36cc6978f18
alej0se <vacaa988@gmail.com>
2023-08-21 01:36:29 -0300

    subiendo el proyecto completo

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ app.get('/', (req, res) => {
     let htmlpath=path.resolve(__dirname, './views/home.html');
     res.sendFile(htmlpath);
 })
+app.get('/registro', (req, res) => {
+    let htmlpath=path.resolve(__dirname, './views/registro.html');
+    res.sendFile(htmlpath);
+})
+app.get('/login',(req,res)=>{
+    let htmlpath=path.resolve(__dirname, './views/login.html');
+    res.sendFile(htmlpath);
+})
